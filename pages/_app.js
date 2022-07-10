c728831d8951aca13589a63347433be9757ed97a
grandpoisson <grandpoisson@gmail.com>
2022-07-10 10:32:45 +0100

    set up initial data model

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import { SessionProvider } from 'next-auth/react'
 
 function MyApp({ Component, pageProps }) {
-  rreturn (
+  return (
     <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
     </SessionProvider>
