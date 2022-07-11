18ecdeb30797951a0cbabff056ad4ed1960a4549
grandpoisson <grandpoisson@gmail.com>
2022-07-11 13:22:28 +0100

    create homepage with list of videos and links

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: [
+      'localhost',
+      'bootcamp-stevec.s3.eu-west-2.amazonaws.com',
+      'bootcamp-stevec.s3.amazonaws.com',
+    ],
+  },
+
 }
 
 module.exports = nextConfig
+
